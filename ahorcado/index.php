7c931ec54869aa6ec560fef12f516784a70381ac
Andres Rojano <arojanomtz@hotmail.com>
2019-12-03 19:31:45 -0600

    2

--- ahorcado/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include ('conf/config.inc.php');
-	echo $tpl->cargar_parte('plt-principal.html', 'encabezado');
-	echo $tpl->cargar_parte('plt-principal.html', 'categoria_s');
-	 echo $tpl->cargar_parte('plt-principal.html', 'pie');
-	 
- 
-?>
